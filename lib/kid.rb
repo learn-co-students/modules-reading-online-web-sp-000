083fb9d794b7ecdc01fa7bd4ad478878127b4d81
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-13 21:25:30 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid < Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
