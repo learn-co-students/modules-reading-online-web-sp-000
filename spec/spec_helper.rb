71155f943a7d50986128dc24b1a57f6f99675c0e
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-27 15:52:17 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/dancer'
-require_relative '../lib/class_methods_module'
-require_relative '../lib/dance_module'
+require_relative '../lib/class_methods_module.rb'
+require_relative '../lib/dance_module.rb'
 require_relative '../lib/kid'
 require_relative '../lib/fancy_dance.rb'
 
