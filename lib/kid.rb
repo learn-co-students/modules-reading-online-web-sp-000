68c6c2e3dd271e89c671e70a1df0228d8c88fdb2
Donna Amos <donnacamos.developer@gmail.com>
2019-03-21 03:09:44 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+class Kid 
+  extend
+  FancyDance::ClassMethods 
+  include 
+  FancyDance::InstanceMethods 
+  
+  def initialize(name)
+    @name = name
+  end 
+end 
