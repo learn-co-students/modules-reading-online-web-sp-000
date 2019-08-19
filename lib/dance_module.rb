dfcc4a734f4c3888a845084737fb9da8f77293b6
Anthony Molina <antonio-molina@att.net>
2019-08-19 02:18:41 +0000

    Done.

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,17 @@
-module MetaDancing 
+module Dance
+  def twirl 
+    "I'm twirling!" 
+  end 
   
-  def metadata 
-    "This class produces objects that love to dance." 
+  def jump 
+    "Look how high I'm jumping!" 
+  end 
+  
+  def pirouette 
+    "I'm doing a pirouette"
+  end 
+  
+  def take_a_bow 
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end 
 end 
