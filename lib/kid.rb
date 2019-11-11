8113bacb864e83ac10f089fd23a35a8583f8f7ab
Michael Carroll <carrollm2@gmail.com>
2019-11-11 23:35:45 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+class Kid
+  include Dance
+  include FancyDance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
