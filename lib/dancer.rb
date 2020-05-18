9908e49567028d12ae14b342f9b7b961476f4fce
Jake G <jake098877@gmail.com>
2020-05-18 17:43:55 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,12 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+require_relative "../lib/kid.rb"
+require_relative "../lib/dancer.rb"
+class Dancer
+  include Dance
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
