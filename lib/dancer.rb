dd68c72eec7f93475a7887cafc68a1df6cf98c55
carriemathieu <carrieburks@gmail.com>
2020-08-17 14:48:21 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Dancer
+    extend FancyDance::ClassMethods # parent module
+    include FancyDance::InstanceMethods # child module
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Kid
+    extend FancyDance::ClassMethods # parent module
+    include FancyDance::InstanceMethods # child module
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
