d81554d5df1eb77bce7c3e9efac30eddbed18efd
Leslie G <lesliegonzalez@me.com>
2020-08-28 23:30:23 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -7,6 +7,9 @@ class Dancer
   include Dance
   extend MetaDancing
   
+  # include FancyDance::InstanceMethods
+  # extend FancyDance::ClassMethods
+  
   attr_accessor :name
   
   def initialize(name)
