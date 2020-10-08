a2b756ef582d414d3522a524dc6db3a65f0963a0
Chandra Lama <iamme24cl@gmail.com>
2020-10-07 19:27:52 -0700

    Done.

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -24,5 +24,17 @@ module FancyDance
             "This class produces objects that love to dance."
         end
     end
-    
 end
+
+class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
+
+
+    
