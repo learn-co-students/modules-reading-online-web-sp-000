7fdf9cfeb54fcef9ce38e1d4bd7a241e9516e0c6
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-07 08:14:34 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative "fancy_dance.rb"
 
 class Dancer
-  include FancyDance::InstanceMethods
-  extend FancyDance::ClassMethods
+  include Dance
+  extend MetaDancing
 
   attr_accessor :name
 
